72a0f6440d9b28f18628c5589cd4e9eed887eb98
Alyson Sanchez <coqui0429@gmail.com>
2024-05-31 13:55:40 -0400

    specifying port

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const fs = require('node:fs');
 const fileStream = require('fs');
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 var jsonTest = JSON.parse(`{"title": "", "text": "t"}`)
 const noteArray = [];
 app.use(express.json());
